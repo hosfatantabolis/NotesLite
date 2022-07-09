bf6d2d88c7b71cb23ebde6eb9011137d1939ee87
Maksim Litvinov <lmk478shade@gmail.com>
2022-07-09 23:08:08 +0300

    feat: todolist add and delete items

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -1,43 +1,44 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 // import { useDispatch } from 'react-redux';
 // import { editNoteAction } from '../../store/noteReducer';
 
 import './ToDoList.css';
 export const ToDoList = ({ selectedNote, setSelectedNote, isVisible }) => {
-    const [value, setValue] = useState('');
-    // const dispatch = useDispatch();
+    const [value, setValue] = useState({});
+    const textInput = useRef();
+
     const handleAddItem = () => {
         setSelectedNote({ ...selectedNote, list: [...selectedNote.list, value] });
-        console.log(selectedNote);
+        setValue({});
+        textInput.current.value = '';
+        console.log(selectedNote.list);
     }
 
     const handleChange = (e) => {
-        setValue(e.target.value);
+        setValue({ ...value, id: Date.now(), text: e.target.value });
     }
 
-    const handleDeleteItem = (index) => {
-        console.log(selectedNote.list[index]);
-
-        setSelectedNote({ ...selectedNote, list: [...selectedNote.list] })
-        // dispatch(editNoteAction())
+    const handleDeleteItem = (e, item) => {
+        console.log(item);
+        setSelectedNote({ ...selectedNote, list: selectedNote.list.filter(listItem => listItem.id !== item.id) })
     }
 
     const handleCheck = (e, item) => {
         console.log(item);
-        setSelectedNote({ ...selectedNote, list: selectedNote.list.filter(item => item.id !== selectedNote) })
+        setSelectedNote({ ...selectedNote, list: selectedNote.list.filter(listItem => listItem.id !== item.id) })
     }
     return (
         <div name="todo" className={`todo ${isVisible ? "" : "todo_hidden"}`}>
-            <input type="text" onChange={(e) => { handleChange(e) }}></input>
+            <input type="text" ref={textInput} onChange={(e) => { handleChange(e) }}></input>
             <button type='button' onClick={handleAddItem}>Add</button>
             <ul className="todo__list">
-                {(selectedNote && selectedNote.list.length > 0) && selectedNote.list.map((item) => {
+                {(selectedNote && selectedNote.list.length > 0) && selectedNote.list.map((item, index) => {
 
                     // console.log(item)
                     return <li key={item.id}>
-                        <input type="checkbox" checked={item.done} onChange={e => { handleCheck(e, item) }}></input>
+                        <input type="checkbox" checked={item.done} onChange={(e) => { handleCheck(e, item) }}></input>
                         {item.text}
-                        <button type="button" onClick={() => { handleDeleteItem() }}>Del</button>
+                        <button type="button" onClick={(e) => { handleDeleteItem(e, item) }}>Del</button>
                     </li>
 
                 })}
