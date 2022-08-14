f1b8b6e24f1e7b6da469a38c7656c115ae518148
Maksim Litvinov <lmk478shade@gmail.com>
2022-08-14 17:47:05 +0300

    bugfix: search in list fix

--- src/store/noteReducer.js
+++ src/store/noteReducer.js
@@ -70,7 +70,7 @@ export const noteReducer = (state = notesArr, action) => {
         case SEARCH_NOTES:
             let newState = Object.assign({}, state);
             const { text } = action.payload;
-            const filteredNotes = state.notes.filter((note) => { return note.text.toLowerCase().includes(text.toLowerCase()) || note.title.toLowerCase().includes(text.toLowerCase()) });
+            const filteredNotes = state.notes.filter((note) => { return note.text.toLowerCase().includes(text.toLowerCase()) || note.title.toLowerCase().includes(text.toLowerCase()) || note.list.some((item) => { return item.text.toLowerCase().includes(text.toLowerCase()) }) });
             if (text) {
                 newState.notes = filteredNotes;
             }
